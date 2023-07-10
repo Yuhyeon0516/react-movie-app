3a7e826ff46ecebdaaea8c560b4680a34a0705a0
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-10 20:46:04 +0900

    router를 활용하여 Home, Detail page로 전환 진행

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({ coverImage, title, summary, genres }) {
+function Movie({ coverImage, title, summary, genres, id }) {
   return (
     <div>
       <img src={coverImage} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
       ) : (
         <div>
           {movies.map((movie) => (
-            <Movie key={movie.id} coverImage={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
+            <Movie key={movie.id} coverImage={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} id={movie.id} />
           ))}
         </div>
       )}
